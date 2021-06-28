1d2c1575bcea77e328dbcf79f396a8c11b91a344
Hong Quan <hohongquan.bk@gmail.com>
2021-06-28 13:47:54 +0200

    init

--- crutransfer-app/src/app/app.component.ts
+++ crutransfer-app/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { ApiRx, WsProvider } from '@polkadot/api';
 import { typesBundleForPolkadot } from '@crustio/type-definitions';
+const IPFS = require('ipfs-core');
 
 const wsProvider = new WsProvider('wss://api.decloudf.com/');
 
@@ -15,15 +16,53 @@ export class AppComponent implements OnInit {
   title = 'crutransfer-app';
 
   api: ApiRx;
+  ipfs: any;
 
-  constructor(){
+
+  constructor() {
 
   }
 
-  ngOnInit(){
+  ngOnInit() {
     ApiRx.create({ provider: wsProvider, typesBundle: typesBundleForPolkadot }).subscribe((api => {
       this.api = api;
-    }))
+      console.log('api', this.api.genesisHash.toHex());
+    }));
+
 
   }
+
+  async onFileSelected(event) {
+
+    const ipfs = await IPFS.create();
+
+    const fileContent: File = event.target.files[0];
+
+    if (fileContent) {
+      console.log('file', fileContent);
+      // Add file into ipfs
+      const fileInfo = await this.addFile(ipfs, fileContent)
+      console.log("File info: " + JSON.stringify(fileInfo));
+
+    }
+  }
+
+  async addFile(ipfs: any, fileContent: any) {
+    // Add file to ipfs
+    const cid = await ipfs.add(
+      fileContent,
+      {
+        progress: (prog) => console.log(`Add received: ${prog}`)
+      }
+    );
+
+    // Get file status from ipfs
+    const fileStat = await ipfs.files.stat("/ipfs/" + cid.path);
+
+    return {
+      cid: cid.path,
+      size: fileStat.cumulativeSize
+    };
+  }
+
 }
