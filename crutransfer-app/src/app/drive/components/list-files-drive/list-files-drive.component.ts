9934b396eb884a7a4d38c3747cc5c1f03b2a7071
hqho <hohongquan.bk@gmail.com>
2021-09-08 20:31:42 +0200

    wip

--- crutransfer-app/src/app/drive/components/list-files-drive/list-files-drive.component.ts
+++ crutransfer-app/src/app/drive/components/list-files-drive/list-files-drive.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { DropzoneComponent } from 'ngx-dropzone-wrapper';
 
 @Component({
   selector: 'app-list-files-drive',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListFilesDriveComponent implements OnInit {
 
+  @ViewChild('dropzone') dropzoneCmp: DropzoneComponent;
+
+  fileToUpload: any;
+  fileErrorMessage: string;
+
+  get fileList(): FileList {
+    return this.dropzoneCmp.directiveRef.dropzone().files;
+  }
+
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  onRemovedfile(event: any) {
+    this.fileErrorMessage = '';
+    if (this.fileList.length == 0) {
+      this.fileToUpload = null;
+    }
+  }
+
+  onFileSelected(event: FileList) {
+    if (event[0] != null && (<any>event[0]).status != 'error') {
+      this.fileErrorMessage = '';
+      this.fileToUpload = event[0];
+    }
+  }
+
+  onFileError(event: any) {
+    this.fileErrorMessage = event[1];
+  }
+
+  tryPin(){
+    
   }
 
 }
--- crutransfer-app/src/app/drive/drive.module.ts
+++ crutransfer-app/src/app/drive/drive.module.ts
@@ -7,6 +7,26 @@ import { SharedModule } from '@cru-transfer/shared';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { FormsModule } from '@angular/forms';
 import { ListFilesDriveComponent } from './components';
+import { DropzoneConfigInterface, DropzoneModule, DROPZONE_CONFIG } from 'ngx-dropzone-wrapper';
+import { BsModalService } from 'ngx-bootstrap/modal';
+
+const DEFAULT_DROPZONE_CONFIG: DropzoneConfigInterface = {
+  maxFiles: 1,
+  url: 'https://httpbin.org/post',
+  thumbnailWidth: 160,
+  maxFilesize: 2000, // 2Go
+  previewTemplate: `<div class="dz-preview dz-file-preview mb-3">
+  <div class="d-flex flex-row "><div class="p-0 w-30 position-relative">
+  <div class="dz-error-mark"><span><i></i></span></div><div class="dz-success-mark">
+  <span><i></i></span></div><div class="preview-container">
+  <img data-dz-thumbnail class="img-thumbnail border-0" />
+  <i class="simple-icon-doc preview-icon" ></i></div>
+  </div><div class="pl-3 pt-2 pr-2 pb-1 w-70 dz-details position-relative"><div><span data-dz-name></span>
+  </div><div class="text-primary text-extra-small" data-dz-size />
+  <div class="dz-progress"><span class="dz-upload" data-dz-uploadprogress></span></div>
+  <div class="dz-error-message"><span data-dz-errormessage></span></div>
+  </div></div><a href="#/" class="remove" data-dz-remove><i class="glyph-icon simple-icon-trash"></i></a></div>`
+};
 
 const routes: Routes = [
   {
@@ -20,6 +40,7 @@ const routes: Routes = [
     CommonModule,
     TranslateModule,
     SharedModule,
+    DropzoneModule,
     FormsModule,
     NgSelectModule,
     RouterModule.forChild(routes)
@@ -27,6 +48,13 @@ const routes: Routes = [
   declarations: [
     DriveComponent,
     ListFilesDriveComponent
+  ],
+  providers: [
+    BsModalService,
+    {
+      provide: DROPZONE_CONFIG,
+      useValue: DEFAULT_DROPZONE_CONFIG
+    }
   ]
 })
 export class DriveModule { }
